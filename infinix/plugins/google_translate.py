8d90cb33e0f2cec86b55191d7eb9bfd95ba000b6
asif2327 <135491102+asif2327@users.noreply.github.com>
2023-06-10 11:19:48 +0530

    Create google_translate.py

---
+++ infinix/plugins/google_translate.py
@@ -0,0 +1,46 @@
+""" Google Translate
+Available Commands:
+.tr LanguageCode as reply to a message
+.tr LanguageCode I text to translate""" 
+
+import emoji
+from googletrans import Translator
+from telethon import events
+from .. import bot
+
+@bot.on(events.NewMessage(incoming=True, pattern="/tr (.*)"))
+async def _(event):
+  if event.fwd_from:
+    return
+  if "trim" in event.raw_text:
+    # https://t.me/c/1220993104/192075
+    return
+  input_str = event.text[4:6]
+  txt = event.text[7:]
+  vtx=await event.reply("Translating...")
+  if event.reply_to_msg_id:
+    previous_message = await vtx.get_reply_message()
+    text = previous_message.message
+    lan = input_str or "en"
+    
+  elif input_str:
+      text = txt 
+      lan = input_str or "en"
+  else:
+      await vtx.edit(".tr LanguageCode as reply to a message")
+      return
+  text = emoji.demojize(text.strip())
+  lan = lan.strip()
+  translator = Translator()
+  translated = translator.translate(text, dest=lan)
+  after_tr_text = translated.text
+    # TODO: emojify the :
+    # either here or before translation
+  output_str = """**Translated By Nsari_bot**\n\nsource **( {} ) **\n\nTranslation *{}""".format(
+      translated.src, lan, after_tr_text
+      )
+  await vtx.edit(output_str)
+      
+    
+    
+ 
